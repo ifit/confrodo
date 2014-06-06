06c969de805861b0e631e390b55baf9a0b2fe814
Scott Hillman <scotth@mediarain.com>
2014-06-06 10:42:43 -0600

    Added ability to provide a custom merging strategy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var optimist = require('optimist')
-  , _        = require('lodash')
-  , argv     = optimist.argv;
+  , argv = optimist.argv;
 
 // optimist provides these, we don't want them.
 delete argv['_'];
@@ -41,10 +40,23 @@ var getEnvironment = function() {
 // Merge two objects together.
 // The second object wins if the both have
 // the same property.
-var mergeConfig = function(configA, configB) {
-  return _.merge(configA, configB, function(a, b) {
-    return _.isArray(a) ? b : undefined;
-  });
+var mergeConfig = function(configA, configB, mergeStrategy) {
+  if (mergeStrategy) {
+    return mergeStrategy(configA, configB);
+  }
+  var merged = {}
+    , hop = Object.prototype.hasOwnProperty;
+  for (var i in configA) {
+    if (hop.call(configA, i)) {
+      merged[i] = configA[i];
+    }
+  }
+  for (var i in configB) {
+    if (hop.call(configB, i)) {
+      merged[i] = configB[i];
+    }
+  }
+  return merged;
 };
 
 // The magic we export.
@@ -52,19 +64,23 @@ var mergeConfig = function(configA, configB) {
 // It is the essence of awesomeness.
 var frodo = function() {
   var args = Array.prototype.slice.call(arguments);
+  var mergeStrategy = null;
+  if (typeof args[args.length - 1] === "function") {
+    mergeStrategy = args.pop();
+  }
   var config = {};
   config.env = getEnvironment();
   for (var i = 0; i < args.length; ++i) {
     if (typeof args[i] === "string") {
       if (args[i] === "ENV") {
-        config = mergeConfig(config, process.env);
+        config = mergeConfig(config, process.env, mergeStrategy);
       } else if (args[i] === "ARGV") {
-        config = mergeConfig(config, argv);
+        config = mergeConfig(config, argv, mergeStrategy);
       } else {
-        config = mergeConfig(config, getFile(args[i]));
+        config = mergeConfig(config, getFile(args[i]), mergeStrategy);
       }
     } else if (typeof args[i] === "object") {
-      config = mergeConfig(config, args[i]);
+      config = mergeConfig(config, args[i], mergeStrategy);
     }
   }
   return config;
